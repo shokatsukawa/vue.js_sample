16ba5c39e56512a19f919f7b6713c410923b4f82
Sho Katsukawa <katsukawa@launchcraft.jp>
2017-08-09 20:17:19 +0900

    Add table sample.

--- script.js
+++ script.js
@@ -19,4 +19,53 @@ $(function() {
 
 
   Select.options = [{val: 'A'}, {val: 'B'}, {val: 'C'}];
+
+  // table ----------------------------------------------------------------------
+
+  var Table = new Vue({
+    el: '#table',
+    components: {
+      'table-input': {
+        props: ['val'],
+        template: '<input type="text" v-on:change="change" :value="val"/>',
+        methods: {
+          // セレクトボックスチェンジイベント
+          change: function (e) {
+            var val = $(e.target).val();
+            alert('select -- val -- ' + val);
+          }
+        }
+      },
+      'message': {
+        props: ['message'],
+        template: '<span>{{ message }}</span>',
+      }
+    },
+    data: {
+      datas: [{val: 'A'}, {val: 'B'}, {val: 'C'}],
+      methods: {
+        // セレクトボックスチェンジイベント
+        change: function (e) {
+          var val = $(e.target).val();
+          alert('select -- val -- ' + val);
+        }
+      }
+    },
+  });
+
+  // table2 --------------------------------------------------------------------
+
+  var Table2 = new Vue({
+    el: '#table2',
+    data: {
+      datas: [{val: 'A'}, {val: 'B'}, {val: 'C'}],
+      methods: {
+        // セレクトボックスチェンジイベント
+        change: function (e) {
+          var val = $(e.target).val();
+          alert('select -- val -- ' + val);
+        }
+      }
+    },
+  });
 });
